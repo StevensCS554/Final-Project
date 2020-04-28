fc993b4d95c10b16ab020fb3bcbff49153ae4499
jsszrxy <jsszrxy@users.noreply.github.com>
2020-04-27 20:58:35 -0400

    config

---
+++ root/backend/config/mongoConnection.js
@@ -0,0 +1,19 @@
+const MongoClient = require("mongodb").MongoClient;
+const settings = require("./settings");
+const mongoConfig = settings.mongoConfig;
+
+let _connection = undefined;
+let _db = undefined;
+
+module.exports = async () => {
+  if (!_connection) {
+    _connection = await MongoClient.connect(mongoConfig.serverUrl,{useUnifiedTopology: true});
+    _db = await _connection.db(mongoConfig.database);
+  }
+
+  return _db;
+};
+
+module.exports.close = async ()=> {
+  _connection.close();
+};
