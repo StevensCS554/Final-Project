d1938e2cd924c0755b9cb2ec38e847b43b8ebafb
kuan lyu <46909761+kuanlv@users.noreply.github.com>
2020-04-29 21:17:07 -0400

    google map api

--- root/frontend/src/components/Explore.js
+++ root/frontend/src/components/Explore.js
@@ -1,10 +1,51 @@
 import React from 'react';
 import Navigation from './utilities/Navigation';
+import Map from './utilities/Map';
+import { withGoogleMap, withScriptjs } from 'react-google-maps';
+
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function Explore() {
+
     return (
         <div>
             <Navigation />
+            <div id='explore-heading'>
+                <p>Check what your neighbors are up to</p>
+            </div>
+            <div className='energy-bar'></div>
+            <div id='google-map' style={{height: '70vh', width: '95%', margin: '1rem auto'}}>
+                <WrappedMap
+                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCTJckDGDyHM8cZ9R-PKUIQGHgfhoXzzFA`}
+                    loadingElement={<div style={{ height: '100%' }} />}
+                    containerElement={<div style={{ height: '100%' }} />}
+                    mapElement={<div style={{ height: '100%' }} />}
+                />
+            </div>
+            <div id='google-map-switches'>
+                <div id='map-on'>
+                    <button onClick={() => {
+                        const map = document.querySelector('#google-map');
+                        const mapOn = document.querySelector('#map-on');
+                        const mapOff = document.querySelector('#map-off');
+                        map.style.height = '70vh';
+                        mapOn.style.display = 'none';
+                        mapOff.style.display = 'block';
+                    }}>OPEN GOOGLE MAP</button>
+                </div>
+                <div id='map-off'>
+                    <button onClick={() => {
+                        const map = document.querySelector('#google-map');
+                        const mapOn = document.querySelector('#map-on');
+                        const mapOff = document.querySelector('#map-off');
+                        map.style.height = '0.3vh';
+                        mapOn.style.display = 'block';
+                        mapOff.style.display = 'none';
+                    }}>CLOSE GOOGLE MAP</button>
+                </div>
+            </div>
+            <div><h1>hehe</h1></div>
         </div>
     )
 }
---
+++ root/frontend/src/components/utilities/Map.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import data from './test.json';
+
+export default function Map() {
+    const [selectedGroup, setSelectedGroup] = useState(null);
+
+    return (
+        <GoogleMap
+            defaultZoom={10}
+            defaultCenter={{ lat: 40, lng: -75 }}
+        >
+            {data.map((d) => (
+                <Marker
+                    key={d.id}
+                    position={{
+                        lat: d.lat,
+                        lng: d.lng
+                    }}
+                    onClick={() => {
+                        setSelectedGroup(d);
+                    }}
+                />
+            ))} 
+            {/* Why there are two pup ups ?!!! */}
+            {selectedGroup && (
+                <InfoWindow 
+                    position={{
+                        lat: selectedGroup.lat,
+                        lng: selectedGroup.lng
+                    }}
+                    onCloseClick={() => {
+                        setSelectedGroup(null);
+                    }}
+                >
+                    <div>Some Details</div>
+                </InfoWindow>
+            )}
+
+        </GoogleMap>
+    )
+}
