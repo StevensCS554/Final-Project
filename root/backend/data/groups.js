dbb33157e47764c402f64d8b75d33e0b3c57b845
jasonx <42896816+xian96@users.noreply.github.com>
2020-05-07 21:00:41 -0400

    Group profile (#9)
    * match the router and data  groups function with the frontend
    
    * group RESTFULL test pass

--- root/backend/data/groups.js
+++ root/backend/data/groups.js
@@ -5,92 +5,114 @@ const { ObjectId } = require("mongodb");
 
 //get all the group
 async function getAll() {
-   const groupsCollection = await groups();
-   const allGroup = groupsCollection.find({});
-   return allGroup;
+    try {
+        const groupsCollection = await groups();
+        const allGroup = groupsCollection.find({});
+        return allGroup;
+    } catch (e) {
+        throw `` + e;
+    }
 };
 
 //get a group by id
 async function getById(id) {
-   checkId(id);
-   const groupsCollection = await groups();
-   const group = groupsCollection.find({ id: id });
-   return group;
+    try {
+        checkId(id);
+        const groupsCollection = await groups();
+        console.log(id);
+        const group = groupsCollection.findOne({ _id: id });
+        return group;
+    } catch (e) {
+        throw `` + e;
+    }
 };
 
-async function creatGroup(name, location, manager, description) {
-   console.log(0);
-
-   const newGroup = {
-      name: name,
-      location: location,
-      manager: manager,
-      user: [],
-      description: description
-   }
-   
-   const groupsCollection = await groups();
-   const insertedGroup = await groupsCollection.insertOne(newGroup);
-   // if(insertedGroup.id)
-   return newGroup;
+async function creatGroup(groupName, groupNotice, maxAge, minAge, gender, maxGroupNo, zipcode, managerId) {
+    try {
+        const newGroup = {
+            groupName: groupName,
+            groupNotice: groupNotice,
+            maxAge: maxAge,
+            minAge: minAge,
+            gender: gender,
+            maxGroupNo: maxGroupNo,
+            zipcode: zipcode,
+            user: [],
+            managerId: managerId,
+        }
+        const groupsCollection = await groups();
+        const insertedGroup = await groupsCollection.insertOne(newGroup);
+        // if(insertedGroup.id)
+        return newGroup;
+    } catch (e) {
+        throw `` + e;
+    }
 };
 
 //update group
-async function updateGroup(id, name = null, location = null, mamager = null, description = null) {
-   try {
-      const checkedId = checkId(id);
+async function updateGroup(id, groupName = null, groupNotice = null, maxAge = null, minAge = null, gender = null, maxGroupNo = null) {
+    try {
+        const checkedId = checkId(id);//double check may not needed;
 
-      //find and update
-      const groupsCollection = await groups();
-      if (name) {
-         const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { name: name } });
-         if (updatedGroup.modifiedCount === 0) throw "Can't Update Name of Group with ID: " + checkedId;
-      }
-      if (location) {
-         const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { location: location } });
-         if (updatedGroup.modifiedCount === 0) throw "Can't Update location of Group with ID: " + checkedId;
-      }
-      if (mamager) {
-         const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { mamager: mamager } });
-         if (updatedGroup.modifiedCount === 0) throw "Can't Update mamager of Group with ID: " + checkedId;
-      }
-      if (description) {
-         const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { description: description } });
-         if (updatedGroup.modifiedCount === 0) throw "Can't Update description of Group with ID: " + checkedId;
-      }
-      return await getById(checkedId);
-   } catch (e) {
-      throw e;
-   }
+        //find and update
+        const groupsCollection = await groups();
+        if (groupName) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { groupName: groupName } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update groupName of Group with ID: " + checkedId;
+        }
+        if (groupNotice) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { groupNotice: groupNotice } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update groupNotice of Group with ID: " + checkedId;
+        }
+        if (maxAge) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { maxAge: maxAge } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update maxAge of Group with ID: " + checkedId;
+        }
+        if (minAge) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { minAge: minAge } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update minAge of Group with ID: " + checkedId;
+        }
+        if (gender) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { gender: gender } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update gender of Group with ID: " + checkedId;
+        }
+        if (maxGroupNo) {
+            const updatedGroup = await groupsCollection.updateOne({ _id: checkedId }, { $set: { maxGroupNo: maxGroupNo } });
+            if (updatedGroup.modifiedCount === 0) throw "Can't Update maxGroupNo of Group with ID: " + checkedId;
+        }
+        return await getById(checkedId);
+    } catch (e) {
+        throw e;
+    }
 };
 
 //delete group 
 async function deleteGroupById(id) {
-   try {
-      const checkedId = checkId(id);
+    try {
+        const checkedId = checkId(id);
 
-      const groupcollection = await groups();
-      const deleteGroup = await groupcollection.deleteOne({ _id: checkedId });
-      if (!deleteGroup.result) throw "Can't Delete Comment with ID: " + checkedId;
-      return true;
-   } catch (e) {
-      throw e;
-   }
+        const groupcollection = await groups();
+        const deleteGroup = await groupcollection.deleteOne({ _id: checkedId });
+        if (!deleteGroup.result) throw "Can't Delete Comment with ID: " + checkedId;
+        return true;
+    } catch (e) {
+        throw e;
+    }
 };
 
 //-----------------------------------check--------------------------------------
 
 function checkId(id) {
-   if (!id) throw "You Must Provide A Id!";
-   if (id._bsontype == "ObjectID") {
-      return id;
-   }
-   else if (typeof id == "string") {
-      return ObjectId(id);
-   }
-   else throw "Input Can't Be An Id!"
+    if (!id) throw "You Must Provide A Id!";
+    if (id._bsontype == "ObjectID") {
+        return id;
+    }
+    else if (typeof id == "string") {
+        return ObjectId(id);
+    }
+    else throw "Input Can't Be An Id!"
 }
 
 module.exports = {
-   getAll, getById, creatGroup, updateGroup, deleteGroupById
+    getAll, getById, creatGroup, updateGroup, deleteGroupById
 };
--- root/backend/routes/groups.js
+++ root/backend/routes/groups.js
@@ -8,145 +8,161 @@ const groupsData = data.groupsData;
 
 //index
 router.get("/", async (req, res) => {
-   try {
-      const allGroup = await groupsData.getAll();
-      res.json(allGroup)
-   } catch (e) {
-      res.status(200).json(e);
-   }
+    try {
+        const allGroup = await groupsData.getAll();
+        res.json(allGroup)
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 //create
 router.post("/", async (req, res) => {
     try {
-        const name = req.body.name;
-        const location = req.body.location;
-        //TODO: 
-        const manager = req.body.manager;
-        //manager should be current user!
-        //using the gived for now
-        // const manager = {
-        //     id: req.user._id,
-        //     username: req.user.username
-        // };
-        const description = req.body.description;
+        //required information:
+        const groupName = req.body.groupName;
+        const groupNotice = req.body.groupNotice;
+        const maxAge = req.body.maxAge;
+        const minAge = req.body.minAge;
+        const gender = req.body.gender;
+        const maxGroupNo = req.body.maxGroupNo;
+        //saved user information:
+        //TODO:
+        // const zipcode = req.zipcode;
+        // const managerId = req.user._id//managerId should be current user!
+        const zipcode = 12345;
+        const managerId = null;
 
-        if (name) {
-            if (typeof name !== 'string') {
-                throw `You Must Provide A Name with string type! what you give:${typeof name}`;
+        if (groupName) {
+            if (typeof groupName !== 'string') {
+                throw `You Must Provide A groupName with string type! what you give:${typeof groupName}`;
             }
         } else {
             throw "You Must Provide A Name!";
         }
-        if (location) {
-            if (typeof location !== 'string') {
-                throw `You Must Provide A location with string type! what you give:${typeof location}`;
+        if (groupNotice) {
+            if (typeof groupNotice !== 'string') {
+                throw `You Must Provide A groupNotice with string type! what you give:${typeof groupNotice}`;
             }
         } else {
-            throw "You Must Provide A location!";
+            throw "You Must Provide A groupNotice!";
         }
-        if (manager) {
-            if (typeof manager !== 'string') {
-                throw `You Must Provide A location with string type! what you give:${typeof manager}`;
+        if (maxAge) {
+            if (isNaN(maxAge)) {
+                throw `You Must Provide A maxAge with number type! what you give:${typeof maxAge}`;
             }
         } else {
-            throw "You Must Provide A location!";
+            throw "You Must Provide A maxAge!";
         }
-        if (description) {
-            if (typeof description !== 'string') {
-                throw `You Must Provide A description with string type! what you give:${typeof description}`;
+        if (minAge) {
+            if (isNaN(minAge)) {
+                throw `You Must Provide A minAge with number type! what you give:${typeof minAge}`;
             }
         } else {
-            throw "You Must Provide A description!";
+            throw "You Must Provide A minAge!";
+        }
+        if (gender) {
+            if (typeof gender !== 'string') {
+                throw `You Must Provide A gender with string type! what you give:${typeof gender}`;
+            }
+        } else {
+            throw "You Must Provide A gender!";
+        }
+        if (maxGroupNo) {
+            if (isNaN(maxGroupNo)) {
+                throw `You Must Provide A maxGroupNo with number type! what you give:${typeof maxGroupNo}`;
+            }
+        } else {
+            throw "You Must Provide A maxGroupNo!";
         }
 
-        const newGroup = await groupsData.creatGroup(name, location, manager, description);
+        const newGroup = await groupsData.creatGroup(groupName, groupNotice, maxAge, minAge, gender, maxGroupNo, zipcode, managerId);
         res.json(newGroup);
     } catch (e) {
-       console.log(e);
         res.status(500).json(e);
     }
 });
 
 //show
 router.get("/:id", async (req, res) => {
-   try {
+    try {
 
-      const id = req.params.id;
-      const checkedId = checkId(id);
+        const id = req.params.id;
+        const checkedId = checkId(id);
 
-      const targetGroup = await groupsData.getById(checkedId);
-      res.json(targetGroup);
-   } catch (e) {
-      res.status(200).json(e);
-   }
+        const targetGroup = await groupsData.getById(checkedId);
+        res.json(targetGroup);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
-
-// // TODO:
-// //edit page
-// router.get("/:id/edit", (req, res) => {
-//     const id = req.params.id;
-//     const checkedId = checkId(id);
-//     const targetGroup = await groupsData.getById(checkedId);
-// });
 
 //update group
 router.put("/:id", async (req, res) => {
-   try {
+    try {
+        const id = req.params.id;
+        const groupName = req.body.groupName;
+        const groupNotice = req.body.groupNotice;
+        const maxAge = req.body.maxAge;
+        const minAge = req.body.minAge;
+        const gender = req.body.gender;
+        const maxGroupNo = req.body.maxGroupNo;
 
-      const id = req.params.id;
-      const name = req.body.name;
-      const location = req.body.location;
-      //TODO: 
-      const manager = req.body.manager;
-      //manager should be current user!
-      //using the gived for now
-      // const manager = {
-      //     id: req.user._id,
-      //     username: req.user.username
-      // };
-      const description = req.body.description;
+        //error check
+        const checkedId = checkId(id);
+        if (groupName) {
+            if (typeof groupName !== 'string') {
+                throw `You Must Provide A groupName with string type! what you give:${typeof groupName}`;
+            }
+        }
+        if (groupNotice) {
+            if (typeof groupNotice !== 'string') {
+                throw `You Must Provide A groupNotice with string type! what you give:${typeof groupNotice}`;
+            }
+        }
+        if (maxAge) {
+            if (isNaN(maxAge)) {
+                throw `You Must Provide A maxAge with number type! what you give:${typeof maxAge}`;
+            }
+        }
+        if (minAge) {
+            if (isNaN(minAge)) {
+                throw `You Must Provide A minAge with number type! what you give:${typeof minAge}`;
+            }
+        }
+        if (gender) {
+            if (typeof gender !== 'string') {
+                throw `You Must Provide A gender with string type! what you give:${typeof gender}`;
+            }
+        }
+        if (maxGroupNo) {
+            if (isNaN(maxGroupNo)) {
+                throw `You Must Provide A maxGroupNo with number type! what you give:${typeof maxGroupNo}`;
+            }
+        }
 
-      //error check
-      const checkedId = checkId(id);
-      if (name) {
-         if (typeof name !== 'string') {
-            throw `You Must Provide A Name with string type! what you give:${typeof name}`;
-         }
-      }
-      if (location) {
-         if (typeof location !== 'string') {
-            throw `You Must Provide A location with string type! what you give:${typeof location}`;
-         }
-      }
-      if (manager) {
-         if (typeof manager !== 'string') {
-            throw `You Must Provide A location with string type! what you give:${typeof manager}`;
-         }
-      }
-      if (description) {
-         if (typeof id !== 'string') {
-            throw `You Must Provide A description with string type! what you give:${typeof id}`;
-         }
-      }
-
-      const updatedGroup = await groupsData.updateGroup(checkedId, name, location, manager, description);
-      res.json(updatedGroup);
-   } catch (e) {
-      res.status(200).json(e);
-   }
+        const updatedGroup = await groupsData.updateGroup(checkedId, groupName, groupNotice, maxAge, minAge, gender, maxGroupNo);
+        res.json(updatedGroup);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 //destory campground route
 router.delete("/:id", async (req, res) => {
-   try {
+    try {
 
-      const id = req.params.id;
-      const checkedId = checkId(id);
+        const id = req.params.id;
+        const checkedId = checkId(id);
 
         const deleted = await groupsData.deleteGroupById(checkedId);
+        if (deleted)
+            res.status(200);
+        else {
+            throw `delete fail` + e;
+        }
     } catch (e) {
-        res.status(200).json(e);
+        res.status(500).json(e);
     }
 
 })
@@ -154,14 +170,14 @@ router.delete("/:id", async (req, res) => {
 //-----------------------------------check--------------------------------------
 //helper
 function checkId(id) {
-   if (!id) throw "You Must Provide A Id!";
-   if (id._bsontype == "ObjectID") {
-      return id;
-   }
-   else if (typeof id == "string") {
-      return ObjectId(id);
-   }
-   else throw "Input Can't Be An Id!"
+    if (!id) throw "You Must Provide A Id!";
+    if (id._bsontype == "ObjectID") {
+        return id;
+    }
+    else if (typeof id == "string") {
+        return ObjectId(id);
+    }
+    else throw "Input Can't Be An Id!"
 }
 
 module.exports = router;
